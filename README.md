113fe50518ecc4bfdefaea93a255ada75290d3fb
WaqasShafi001 <waqasshafi001@gmail.com>
2025-07-27 00:09:56 +0500

    first commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# hero_animation_tip
+
+A new Flutter project.
---
+++ android/app/src/main/kotlin/com/example/hero_animation_tip/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.hero_animation_tip
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity : FlutterActivity()
